2955de7d1536726dc5c21a1193aaa1c60076727f
NicolaiStephansen <nicostephansen@gmail.com>
2025-09-22 12:52:25 +0200

    Kommentarer på ErrorViewModel.cs og ObstacleData.cs

--- FirstWebApplication1/Models/ErrorViewModel.cs
+++ FirstWebApplication1/Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
-namespace FirstWebApplication1.Models
+namespace FirstWebApplication1.Models //namespace for å organisere koden, alle models i prosjektet samles her
 {
-    public class ErrorViewModel
+    public class ErrorViewModel //Klasse som representerer feilmelding, hvis noe går galt får brukeren feilmelding
     {
-        public string? RequestId { get; set; }
+        public string? RequestId { get; set; } //Gir en ID som viser hvilken forespørsel feilen skjedde på,? betyr at verdien kan være null
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId); // sjekker om Requestid finnes, bool gir true or false, hvis Requestid har verdi (true), kan vises, hvis (false), kan ikke vises
     }
 }
