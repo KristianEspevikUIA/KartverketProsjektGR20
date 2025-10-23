e30b40721ba7e14b71512bbec61ee55a68afb9cf
endim05 <endim@uia.no>
2025-10-23 14:29:36 +0200

    Lagt til manglede filer i unittest
    fikset problemet

---
+++ FirstWebApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using FirstWebApplication1.Models;
+
+namespace FirstWebApplication1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Obstacle_ShouldHaveDefaultValues()
+        {
+            var obstacle = new ObstacleData();
+            Assert.NotNull(obstacle);
+            Assert.Equal(0, obstacle.Id);
+        }
+    }
+}
